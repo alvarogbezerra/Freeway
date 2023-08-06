4d8242d6782030da7fd647efe8066346e7350790
Álvaro Bezerra <120674321+alvarogbezerra@users.noreply.github.com>
2023-08-06 10:06:19 -0300

    Add files via upload

---
+++ ator.js
@@ -0,0 +1,59 @@
+//código do ator
+let xAtor = 100;
+let yAtor = 366;
+let colisao = false;
+let pontuação = 0
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    if (podeSeMover())
+    yAtor += 3;
+  }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+    voltaPosiçãoInicial ();
+    }
+  }
+}
+
+function voltaPosiçãoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+textSize(32);
+text(pontuação, 200, 30);
+fill(0, 102, 153);
+}
+
+function marcaPonto(){
+ if (yAtor < 31)
+   pontuação += 1;
+   voltaPosiçãoInicial();
+ if  (colisao)
+   pontuação = 0
+}
+
+function podeSeMover(){
+  return yAtor < 366;
+}
+
+
+
+
+
+
+
+
